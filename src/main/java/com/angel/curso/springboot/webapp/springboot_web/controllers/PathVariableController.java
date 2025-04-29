81aa3cfa5a57304d7ee1ef4c8d2a933c4a81914a
Angel <aramirezd17@gmail.com>
2025-04-28 20:06:32 -0600

    PathVariable Mix

--- src/main/java/com/angel/curso/springboot/webapp/springboot_web/controllers/PathVariableController.java
+++ src/main/java/com/angel/curso/springboot/webapp/springboot_web/controllers/PathVariableController.java
@@ -1,12 +1,15 @@
 package com.angel.curso.springboot.webapp.springboot_web.controllers;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.angel.curso.springboot.webapp.springboot_web.models.dto.ParamDto;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 
 
 @RestController
@@ -20,6 +23,15 @@ public class PathVariableController {
 
         return param;
     }
+
+    @GetMapping("/mix/{product}/{id}")
+    public Map<String, Object> mixPathVar(@PathVariable String product, @PathVariable Long id) {
+        Map<String, Object> json = new HashMap<>();
+        json.put("producto", product);
+        json.put("id", id); 
+        return json;
+    }
+    
     
 
 }
