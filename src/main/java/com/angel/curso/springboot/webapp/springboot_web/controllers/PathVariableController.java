7a1c80e26fe1b3cfc8aa34dcd514c5c1df22d8f9
Angel <aramirezd17@gmail.com>
2025-04-28 21:12:53 -0600

    Post en PathVariable

--- src/main/java/com/angel/curso/springboot/webapp/springboot_web/controllers/PathVariableController.java
+++ src/main/java/com/angel/curso/springboot/webapp/springboot_web/controllers/PathVariableController.java
@@ -5,10 +5,14 @@ import java.util.Map;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.angel.curso.springboot.webapp.springboot_web.models.User;
 import com.angel.curso.springboot.webapp.springboot_web.models.dto.ParamDto;
+
 
 
 
@@ -31,7 +35,40 @@ public class PathVariableController {
         json.put("id", id); 
         return json;
     }
+
+
+    @PostMapping("/create")
+    public User postUser(@RequestBody User user) {
+        //Hacer algo con el usuario save en la bbdd 
+        user.setName(user.getName().toUpperCase());
+        return user;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+}
+    
     
     
 
-}
